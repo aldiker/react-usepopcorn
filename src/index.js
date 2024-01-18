ce781f051664ef5f0b5cad35810cc42f8402e83f
aldiker <aldiker@ukr.net>
2024-01-18 15:03:16 +0200

    Created StartRating

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import StarRating from './components/StarRating'
 
-import App from './App'
-import './index.css'
+// import App from './App'
+// import './index.css'
 
 const rootElement = document.getElementById('root')
 const root = createRoot(rootElement)
 
 root.render(
     <StrictMode>
-        <App />
+        {/* <App /> */}
+        <StarRating maxRating={10} />
+        <StarRating />
     </StrictMode>
 )
