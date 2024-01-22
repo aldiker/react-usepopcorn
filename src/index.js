f32f5b5e7cc11925b71ea70ad3ea2b963adb39b7
aldiker <aldiker@ukr.net>
2024-01-22 16:58:50 +0200

    Added a lot of props to StarRating. Used PropTypes to control property types.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
-import { StrictMode } from 'react'
+import { StrictMode, useState } from 'react'
 import { createRoot } from 'react-dom/client'
 import StarRating from './components/StarRating'
 
 // import App from './App'
 // import './index.css'
+
+function Test() {
+    const [movieRating, setMovieRating] = useState(0)
+
+    return (
+        <div>
+            <StarRating
+                color='blue'
+                maxRating={10}
+                onSetRating={setMovieRating}
+            />
+            <p>This movie was rated {movieRating} stars</p>
+        </div>
+    )
+}
 
 const rootElement = document.getElementById('root')
 const root = createRoot(rootElement)
@@ -11,7 +26,14 @@ const root = createRoot(rootElement)
 root.render(
     <StrictMode>
         {/* <App /> */}
-        <StarRating maxRating={10} />
-        <StarRating />
+        <StarRating maxRating={7} />
+        {/* <StarRating size={24} color={'red'} />
+        <StarRating
+            maxRating={5}
+            messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing!']}
+        />
+
+        <StarRating defaultRating={'test'} />
+        <Test /> */}
     </StrictMode>
 )
