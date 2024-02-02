59cc5566095952f55d4794e1a4492a7257a32bb9
aldiker <aldiker@ukr.net>
2024-02-02 15:26:42 +0200

    Changed MovieDetails

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -1,7 +1,13 @@
 import { useEffect, useState } from 'react'
+import StarRating from './StarRating'
+import Loader from './Loader'
+import ErrorMessage from './ErrorMessage'
 
 export default function MovieDetails({ selectedId, onCloseMovie, KEY }) {
     const [movie, setMovie] = useState({})
+    const [isLoading, setIsLoading] = useState(false)
+    const [error, setError] = useState('')
+
     const {
         Title: title,
         Year: year,
@@ -18,12 +24,22 @@ export default function MovieDetails({ selectedId, onCloseMovie, KEY }) {
     useEffect(
         function () {
             async function getMovieDetais() {
-                const res = await fetch(
-                    `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
-                )
-                const data = await res.json()
-                console.log(data)
-                setMovie(data)
+                try {
+                    setIsLoading(true)
+                    setError('')
+
+                    const res = await fetch(
+                        `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+                    )
+                    const data = await res.json()
+                    console.log(data)
+                    setMovie(data)
+                } catch (err) {
+                    console.log(err.message)
+                    setError(err.message)
+                } finally {
+                    setIsLoading(false)
+                }
             }
             if (!selectedId) return
             getMovieDetais()
@@ -32,27 +48,40 @@ export default function MovieDetails({ selectedId, onCloseMovie, KEY }) {
     )
 
     return (
-        <div className='details' onClick={onCloseMovie}>
-            <header>
-                <img src={poster} alt={`Poster of ${movie} movie`} />
-                <div className='details-overview'>
-                    <h2>{title}</h2>
-                    <p>
-                        {released} &bull; {runtime}
-                    </p>
-                    <p>{genre}</p>
-                    <p>
-                        <span>⭐</span> {imdbRating} IMDb rating
-                    </p>
-                </div>
-            </header>
-            <section>
-                <p>
-                    <em>{plot}</em>
-                </p>
-                <p>Starring {actors}</p>
-                <p>Directed by {director}</p>
-            </section>
+        <div className='details'>
+            {isLoading ? (
+                <Loader />
+            ) : (
+                <>
+                    <header>
+                        <button className='btn-back' onClick={onCloseMovie}>
+                            ⇦
+                        </button>
+                        <img src={poster} alt={`Poster of ${movie} movie`} />
+                        <div className='details-overview'>
+                            <h2>{title}</h2>
+                            <p>
+                                {released} &bull; {runtime}
+                            </p>
+                            <p>{genre}</p>
+                            <p>
+                                <span>⭐</span> {imdbRating} IMDb rating
+                            </p>
+                        </div>
+                    </header>
+                    <section>
+                        <div className='rating'>
+                            <StarRating maxRating={10} size={24} />
+                        </div>
+                        <p>
+                            <em>{plot}</em>
+                        </p>
+                        <p>Starring {actors}</p>
+                        <p>Directed by {director}</p>
+                    </section>
+                </>
+            )}
+            {error && <ErrorMessage message={error} />}
         </div>
     )
 }
